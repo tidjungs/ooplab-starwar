41b03cabe0d919dcb2ddbe407816c7dfc5faff62
tidjungs <waewprach.s@ku.th>
2016-12-26 19:25:05 +0700

    add world

--- models.py
+++ models.py
@@ -1,9 +1,20 @@
 class Ship:
-    def __init__(self, x, y):
+    def __init__(self, world, x, y):
+        self.world = world
         self.x = x
         self.y = y
- 
+
     def animate(self, delta):
-        if self.y > 600:
+        if self.y > self.world.height:
             self.y = 0
         self.y += 5
+
+class World:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        self.ship = Ship(self, 100, 100)
+
+    def animate(self, delta):
+        self.ship.animate(delta)
--- starwar.py
+++ starwar.py
@@ -1,5 +1,5 @@
 import arcade
-from models import Ship
+from models import World, Ship
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
@@ -10,7 +10,7 @@ class StarwarGameWindow(arcade.Window):
 
         arcade.set_background_color(arcade.color.BLACK)
 
-        self.ship = Ship(100,100)
+        self.world = World(width, height)
         self.ship_sprite = arcade.Sprite('images/ship.png')
 
     def on_draw(self):
@@ -18,10 +18,8 @@ class StarwarGameWindow(arcade.Window):
         self.ship_sprite.draw()
 
     def animate(self, delta):
-        ship = self.ship
-
-        ship.animate(delta)
-        self.ship_sprite.set_position(ship.x, ship.y)
+        self.world.animate(delta)
+        self.ship_sprite.set_position(self.world.ship.x, self.world.ship.y)
 
 if __name__ == '__main__':
     window = StarwarGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
