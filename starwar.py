05b878c86cc397eaf4ac7f582d5a7997aadfc774
tidjungs <waewprach.s@ku.th>
2016-12-26 19:22:39 +0700

    moving ship & add models

--- starwar.py
+++ starwar.py
@@ -1,22 +1,28 @@
 import arcade
- 
+from models import Ship
+
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
- 
+
 class StarwarGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
- 
+
         arcade.set_background_color(arcade.color.BLACK)
 
-        self.ship = arcade.Sprite('images/ship.png')
-        self.ship.set_position(100, 100)
- 
+        self.ship = Ship(100,100)
+        self.ship_sprite = arcade.Sprite('images/ship.png')
+
     def on_draw(self):
         arcade.start_render()
-        self.ship.draw()
- 
- 
+        self.ship_sprite.draw()
+
+    def animate(self, delta):
+        ship = self.ship
+
+        ship.animate(delta)
+        self.ship_sprite.set_position(ship.x, ship.y)
+
 if __name__ == '__main__':
     window = StarwarGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run()
