ab1d317d1c29f8f443abf0492730578d38ee42b5
tidjungs <waewprach.s@ku.th>
2016-12-26 19:17:15 +0700

    render ship

--- starwar.py
+++ starwar.py
@@ -1,6 +1,6 @@
 import arcade
  
-SCREEN_WIDTH = 600
+SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
  
 class StarwarGameWindow(arcade.Window):
@@ -8,10 +8,13 @@ class StarwarGameWindow(arcade.Window):
         super().__init__(width, height)
  
         arcade.set_background_color(arcade.color.BLACK)
- 
+
+        self.ship = arcade.Sprite('images/ship.png')
+        self.ship.set_position(100, 100)
  
     def on_draw(self):
         arcade.start_render()
+        self.ship.draw()
  
  
 if __name__ == '__main__':
