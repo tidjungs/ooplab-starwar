1367c1d730a67ae3aa00eee8c387e6fc066aa31f
tidjungs <waewprach.s@ku.th>
2016-12-26 19:29:00 +0700

    add model sprite

--- starwar.py
+++ starwar.py
@@ -4,6 +4,20 @@ from models import World, Ship
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
 class StarwarGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
@@ -11,7 +25,7 @@ class StarwarGameWindow(arcade.Window):
         arcade.set_background_color(arcade.color.BLACK)
 
         self.world = World(width, height)
-        self.ship_sprite = arcade.Sprite('images/ship.png')
+        self.ship_sprite = ModelSprite('images/ship.png',model=self.world.ship)
 
     def on_draw(self):
         arcade.start_render()
@@ -19,7 +33,6 @@ class StarwarGameWindow(arcade.Window):
 
     def animate(self, delta):
         self.world.animate(delta)
-        self.ship_sprite.set_position(self.world.ship.x, self.world.ship.y)
 
 if __name__ == '__main__':
     window = StarwarGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
